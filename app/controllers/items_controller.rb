a8802b74ffbcde4b650879d94ee758b522bf9a80
Renan Antonio da Silva <renanras@gmail.com>
2023-05-10 20:33:21 -0300

    Cadastro de Item apenas por Admin.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :update, :show]
-
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @items = Item.all
@@ -19,11 +20,33 @@ class ItemsController < ApplicationController
     if @item.save()
       redirect_to items_path, notice: 'Item cadastrado com sucesso.'
     else
-      flash.now[:notice] = 'Item não cadastrado.'
+      flash.now[:alert] = 'Item não cadastrado.'
       render 'new'
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item atualizado com sucesso'
+    else
+      flash.now[:alert] = "Item não atualizado"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    if @item.destroy
+      flash[:notice] = 'Item removido com sucesso'
+    else
+      flash[:alert] = "Erro: Item não removido"
+    end
+    redirect_to items_path
+  end
+
+
   private
 
   def set_item
@@ -31,7 +54,14 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :image, :weight, :width, :height, :depth, :category)
+    params.require(:item).permit(:name, :description, :weight, :width, :height, :depth, :category, :image)
   end
   
+  def require_admin
+    unless current_user.admin?
+      flash[:alert] = "Apenas administradores podem realizar esta ação."
+      redirect_to items_path
+    end
+  end
+
 end
