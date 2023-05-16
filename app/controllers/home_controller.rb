5b48fc2fef266abf779d8008ef6ff18da07bec0f
Renan Antonio da Silva <renanras@gmail.com>
2023-05-15 21:17:12 -0300

    Busca de Produtos por Item.name ou Lot.code resultados parciais %LIKE%

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @open_auctions_lots = Lot.open_auctions
-    @future_auctions_lots = Lot.future_auctions
+    if params[:search].present?
+      @open_auctions_lots = Lot.open_auctions.joins(:items).where("lots.code LIKE :search OR items.name LIKE :search", search: "%#{params[:search]}%").distinct
+      @future_auctions_lots = Lot.future_auctions.joins(:items).where("lots.code LIKE :search OR items.name LIKE :search", search: "%#{params[:search]}%").distinct
+    else
+      @open_auctions_lots = Lot.open_auctions
+      @future_auctions_lots = Lot.future_auctions
+    end
   end
 end
