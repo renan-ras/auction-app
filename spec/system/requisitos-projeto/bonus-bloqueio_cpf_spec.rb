229edd844a44a999ee1f1d066cd9895d2e50368f
Renan Antonio da Silva <renanras@gmail.com>
2023-05-17 19:00:18 -0300

    Corrigida inconsistência no DB; Admin deletado

---
+++ spec/system/requisitos-projeto/bonus-bloqueio_cpf_spec.rb
@@ -0,0 +1,4 @@
+#Usuários administradores podem registrar um bloqueio para CPF. Um CPF bloqueado não pode se cadastrar no sistema. Caso já possua cadastro, ao fazer login deve ser exibida uma mensagem informando que sua conta está suspensa. Usuários com CPF bloqueado não podem fazer lances em leilões nem enviar novas dúvidas para um lote.
+
+require 'rails_helper'
+
