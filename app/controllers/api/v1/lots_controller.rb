14335e6573688a3078e144abd6c5f349c9d95b89
Renan Antonio da Silva <renanras@gmail.com>
2023-05-30 22:33:29 -0300

    API lot#index

--- app/controllers/api/v1/lots_controller.rb
+++ app/controllers/api/v1/lots_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::LotsController < ActionController::API
   def show
-    lot = Lot.find(params[:id])
-    render status: 200, json: lot
+    begin
+      lot = Lot.find(params[:id])
+      render status: 200, json: lot.as_json(except: [:created_at, :updated_at])
+    rescue
+      render status: 404
+    end
   end
   
+  def index
+    lots = Lot.all.order(:code)
+    render status: 200, json: lots.as_json(except: [:created_at, :updated_at])
+  end
+
 end
--- spec/requests/apis/lot_api_spec.rb
+++ spec/requests/apis/lot_api_spec.rb
@@ -20,5 +20,56 @@ describe "Lot API" do
       expect(json_response["id"]).to eq lot_a.id
       # expect(json_response).to include 'banana'
     end
+
+    it "fail" do
+      ##
+      ##
+      get "/api/v1/lots/999999"
+
+      ##
+      expect(response.status).to  eq 404
+    end
+    
   end
+
+  context "GET /api/v1/lots" do
+    it "in code order" do
+      ##
+      admin_a = User.create!(nickname: 'Ronaldo', email: 'fenomeno@leilaodogalpao.com.br', password: '123456', cpf: '69142235219')
+      lot_a = Lot.create!(code: 'XYZ123456', start_date: 1.day.from_now, end_date: 3.days.from_now, minimum_bid: 100, minimum_bid_increment: 10, creator: admin_a)
+      lot_b = Lot.create!(code: 'ABC123456', start_date: 1.day.from_now, end_date: 3.days.from_now, minimum_bid: 100, minimum_bid_increment: 10, creator: admin_a)
+
+      ##
+      get "/api/v1/lots"
+
+      ##
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response[0]["code"]).to eq 'ABC123456'
+      expect(json_response[1]["code"]).to eq 'XYZ123456'
+      # expect(json_response).to include 'banana'
+    end
+
+    it "empty" do
+      ##
+
+      ##
+      get "/api/v1/lots"
+
+      ##
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response).to eq []
+      # expect(json_response).to include 'banana'
+    end
+    
+    
+  end
+  
 end
